5c03b471579147bd570f1a322497e32093800134
Bjarne Tvedten <bjarnet3@hotmail.com>
2018-05-21 20:17:52 +0200

    Added iOS

---
+++ iOS/TV Remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV Remote
+//
+//  Created by Bjarne Tvedten on 21.05.2018.
+//  Copyright © 2018 Digital Mood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
